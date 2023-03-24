963570025a22de12cf2a86ee39f7080840d6c93b
Payam <105701220+kshoj@users.noreply.github.com>
2023-03-24 01:50:03 -0400

    Write a function that gets the computer's choice

---
+++ script.js
@@ -0,0 +1,7 @@
+function getComputerChoice() {
+
+    const choices = ["Rock", "Paper", "Scissor"];
+
+    const random = Math.floor(Math.random() * choices.length);
+    console.log(months[random]);
+}
