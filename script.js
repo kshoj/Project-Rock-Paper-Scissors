722ca6803d2b1e3541543728e62026460c7fa939
Payam <105701220+kshoj@users.noreply.github.com>
2023-03-24 03:19:12 -0400

    Write a function that plays a single round

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 function getComputerChoice() {
 
-    const choices = ["Rock", "Paper", "Scissor"];
+    const choices = ["rock", "paper", "scissor"];
 
     const random = Math.floor(Math.random() * choices.length);
-    computerSelection = choices[random];
-    console.log(computerSelection);
+    console.log(choices[random]);
+    return(choices[random]);
 }
 
 let playerSelection = prompt('What is your weapon of choice?');
+playerSelection = playerSelection.toLowerCase()
 let computerSelection = getComputerChoice();
+
+function playRound() {
+    if (playerSelection == computerSelection) {
+        console.log('it\'s a tie!');
+    
+    } else if (playerSelection == 'rock') {
+        if (computerSelection == 'paper') {
+            console.log('you lost!');
+        }else {
+            console.log('you won!');
+        }
+    } else if (playerSelection =='paper')
+    if (computerSelection == 'scissor') {
+        console.log('you lost!');
+    }else {
+        console.log('you won!');
+    } else if (playerSelection =='scissor')
+    if (computerSelection == 'rock') {
+        console.log('you lost!');
+    }else {
+        console.log('you won!');
+    }
+}
+
+playRound();
+
 
