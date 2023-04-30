8f89cca8cb9557458f4102a11d39c4f39a43ceb9
Payam <105701220+kshoj@users.noreply.github.com>
2023-04-30 04:12:54 -0400

    Add basic UI to the game.

--- script.js
+++ script.js
@@ -7,57 +7,76 @@ function getComputerChoice() {
     return(choices[random]);
 }
 
-function playRound() {
+let games = 0
+let wins = 0;
+let losses = 0;
+let ties = 0;
 
-    let playerSelection = prompt('What is your weapon of choice?');
-    playerSelection = playerSelection.toLowerCase()
+function playRound(playerSelection) {
     let computerSelection = getComputerChoice();
-
+  
     if (playerSelection === computerSelection) {
-        console.log('it\'s a tie round!');
-    
-    } else if (playerSelection === 'rock') {
-        if (computerSelection === 'paper') {
-            console.log('you lost this round!');
-            return false;
-        }else {
-            console.log('you won this round!');
-            return true;
-        }
-    } else if (playerSelection === 'paper')
-        if (computerSelection === 'scissor') {
-            console.log('you lost this round!');
-            return false;
-        }else {
-            console.log('you won this round!');
-            return true;
-    } else if (playerSelection === 'scissor')
-        if (computerSelection === 'rock') {
-            console.log('you lost this round!');
-            return false;
-        }else {
-            console.log('you won this round!');
-            return true;
-    }
-}
-function game() {
-    wins = 0;
-    losses = 0;
-    for (i = 0; i < 5; i++) {
-        const roundResult = playRound();
-        if (roundResult === true) {
-          wins++;
-        } else if (roundResult === false) {
-          losses++;
-        }
+      round.textContent = "it's a tie round!";
+      ties++;
+    } else if (playerSelection === "rock") {
+      if (computerSelection === "paper") {
+        round.textContent = "you lost this round!";
+        losses++;
+      } else {
+        round.textContent = "you won this round!";
+        wins++;
       }
-      
-
-    if (wins > losses) {
-        console.log ('you won the game!');
-    } else if (losses > wins) {
-        console.log ('you lost the game!');
-    } else {
-        console.log ('it\'s a tie game!');
+    } else if (playerSelection === "paper") {
+      if (computerSelection === "scissor") {
+        round.textContent = "you lost this round!";
+        losses++;
+      } else {
+        round.textContent = "you won this round!";
+        wins++;
+      }
+    } else if (playerSelection === "scissor") {
+      if (computerSelection === "rock") {
+        round.textContent = "you lost this round!";
+        losses++;
+      } else {
+        round.textContent = "you won this round!";
+        wins++;
+      }
     }
-}
+    games++;
+    score.textContent = `Human: ${wins}, Machine: ${losses}, Ties: ${ties}`;
+    game();
+  }
+
+function game() {
+    if (games >= 5) {
+        if (wins > losses) {
+            gameResult.textContent = "You won the game!";
+          } else if (losses > wins) {
+            gameResult.textContent = "You lost the game!";
+          } else {
+            gameResult.textContent = "It's a tie game!";
+          }
+        wins = 0;
+        losses = 0;
+        ties = 0;
+        games = 0;
+
+
+    }
+
+  }
+
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissor = document.querySelector('#scissor');
+const round = document.querySelector('#result');
+const score = document.querySelector('#score');
+const gameResult = document.querySelector('#gameResult');
+const btn = document.querySelector('button');
+
+
+rock.addEventListener('click' , function() {playRound('rock');});
+paper.addEventListener('click' , function() {playRound('paper');});
+scissor.addEventListener('click' , function() {playRound('scissor');});
+button.addEventListener('click' , game);
